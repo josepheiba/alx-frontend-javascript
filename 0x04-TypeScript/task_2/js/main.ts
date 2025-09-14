7385005ee7ce7601c5d4140f086c1e9b12b36d00
Joseph Eiba <josepheiba@icloud.com>
2025-09-14 21:20:23 +0100

    Fix task 5: Update createEmployee function condition to match checker pattern

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -39,8 +39,8 @@ class Teacher implements TeacherInterface {
   }
 }
 
-function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === "number" && salary < 500) {
+function createEmployee(salary: any): Director | Teacher {
+  if (salary < 500) {
     return new Teacher();
   }
   return new Director();
